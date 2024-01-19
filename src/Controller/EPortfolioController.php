0d7da78a8fd2515490737c6e2efa8a4fafda2bc1
vhxpn0tick <mathieu.bovy@etu.univ-st-etienne.fr>
2024-01-19 10:18:15 +0100

    MAJ

--- src/Controller/EPortfolioController.php
+++ src/Controller/EPortfolioController.php
@@ -15,4 +15,40 @@ class EPortfolioController extends AbstractController
             'controller_name' => 'EPortfolioController',
         ]);
     }
+    #[Route('/infos', name: 'app_infos')]
+    public function infos(): Response
+    {
+        return $this->render('e_portfolio/apropos.html.twig', [
+            'controller_name' => 'EPortfolioController',
+        ]);
+    }
+    #[Route('/CV', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('e_portfolio/CV.html.twig', [
+            'controller_name' => 'EPortfolioController',
+        ]);
+    }
+    #[Route('/RT1', name: 'app_rt1')]
+    public function rt1(): Response
+    {
+        return $this->render('e_portfolio/RT1.html.twig', [
+            'controller_name' => 'EPortfolioController',
+        ]);
+    }
+    #[Route('/RT2', name: 'app_rt2')]
+    public function rt2(): Response
+    {
+        return $this->render('e_portfolio/RT2.html.twig', [
+            'controller_name' => 'EPortfolioController',
+        ]);
+    }
+    #[Route('/RT3', name: 'app_rt3')]
+    public function RT3(): Response
+    {
+        return $this->render('e_portfolio/RT3.html.twig', [
+            'controller_name' => 'EPortfolioController',
+        ]);
+    }
+
 }
